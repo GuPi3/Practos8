478fbd5f48764a26c9409efcb900dfe6b5bbb6f8
GuPi_3 <GuPi_3@DESKTOP-2DCLU9G>
2021-12-09 00:02:26 +0300

    Добавьте файлы проекта.

---
+++ Practos8/Iworker.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practos8
+{
+    interface Iworker
+    {
+        public int Payment { get; set; }
+        public string Name { get; set; }
+        public string GetInfo();
+        public int Pay();
+    }
+    class Salary : Iworker, IComparable
+    {
+
+        public int Payment { get; set; }
+        public string Name { get; set; }
+        public string GetInfo()
+        {
+            return "Работник:" + Name + "\nЗарплата:" + Payment;
+        }
+        public int Pay()
+        {
+            return Payment;
+        }
+        public Salary(int salary, string name)
+        {
+            Name = name;
+            Payment = salary;
+        }
+        public int CompareTo(object obj)
+        {
+            Salary salary = (Salary)obj;
+            if (Convert.ToChar(Name.Substring(0, 1)) < Convert.ToChar(salary.Name.Substring(0, 1))) return 1; 
+            if (Convert.ToChar(Name.Substring(0,1)) > Convert.ToChar(salary.Name.Substring(0,1))) return -1;
+            return 0;
+        }
+    }
+    class HourlyPayment : Iworker, IComparable
+    {
+        public int Payment { get; set; }
+        public string Name { get; set; }
+        public string GetInfo()
+        {
+            return "Работник:" + Name + "\nЗарплата в час:" + Payment;
+        }
+        public int Pay() 
+        {
+
+            return Payment;
+        }
+        public HourlyPayment(int salary, string name)
+        {
+            Name = name;
+            Payment = salary;
+        }
+        public int CompareTo(object obj)
+        {
+            Salary salary = (Salary)obj;
+            if (Convert.ToChar(Name.Substring(0, 1)) < Convert.ToChar(salary.Name.Substring(0, 1))) return 1;
+            if (Convert.ToChar(Name.Substring(0, 1)) > Convert.ToChar(salary.Name.Substring(0, 1))) return -1;
+            return 0;
+        }
+    }
+}
